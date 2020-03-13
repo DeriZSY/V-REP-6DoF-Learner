8207239d90a237dc155d84a8d5c210652e73cb08
zhangsiyu <2013207520@qq.com>
2020-03-13 22:02:45 +0800

    helper method implemented

---
+++ utils.py
@@ -0,0 +1,29 @@
+import numpy as np
+import math
+
+
+def euler2rotm(theta):
+    R_x = np.array([[1,         0,                  0                   ],
+                    [0,         math.cos(theta[0]), -math.sin(theta[0]) ],
+                    [0,         math.sin(theta[0]), math.cos(theta[0])  ]
+                    ])
+    R_y = np.array([[math.cos(theta[1]),    0,      math.sin(theta[1])  ],
+                    [0,                     1,      0                   ],
+                    [-math.sin(theta[1]),   0,      math.cos(theta[1])  ]
+                    ])
+    R_z = np.array([[math.cos(theta[2]),    -math.sin(theta[2]),    0],
+                    [math.sin(theta[2]),    math.cos(theta[2]),     0],
+                    [0,                     0,                      1]
+                    ])
+    R = np.dot(R_z, np.dot( R_y, R_x ))
+    return R
+
+
+def save_rgb(rgb_img, rgb_path):
+    from PIL import Image
+    im = Image.fromarray(rgb_img)
+    im.save(rgb_path)
+
+
+def save_depth(depth_img, depth_path):
+    np.save(depth_path, depth_img)
